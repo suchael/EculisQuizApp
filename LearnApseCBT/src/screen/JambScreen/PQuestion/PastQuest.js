16ec399f17921ca622a2b42417b4f27f0529a07a
Eculis <succhycomic@gmail.com>
2023-08-15 02:18:18 +0100

    added a toggle botton instructions to General Past Questions

--- LearnApseCBT/src/screen/JambScreen/PQuestion/PastQuest.js
+++ LearnApseCBT/src/screen/JambScreen/PQuestion/PastQuest.js
@@ -1,8 +1,9 @@
 import {View, 
         Text, 
         StyleSheet,
-        TouchableHighlight } from 'react-native'
-import React from 'react';
+        TouchableHighlight } from 'react-native';
+
+import React , {useState}from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useNavigation } from '@react-navigation/native';
@@ -85,25 +86,67 @@ function TabBar(){
         },
     }}>
       <Tab.Screen name ="General" component={SelectByGeneral}/>
-      <Tab.Screen name ="By Topic" component={SelectByTopic}/>
+      <Tab.Screen name ="Topic Based" component={SelectByTopic}/>
     </Tab.Navigator>
   );
 }
 
 function SelectByGeneral(){
+  const insets = useSafeAreaInsets();
+  const instruction = "Note: Questions from 2015 and above have been compiled from students who sat for the exam, as JAMB halted the issuance of compiled questions.";
   return(
-    <View>
-      <Text>General</Text>
+    <View style = {{
+      paddingLeft: insets.left + 10,
+      paddingRight: insets.right + 10,
+      paddingTop: insets.top + 15,
+      paddingBottom: insets.bottom + 12,
+  }}>
+      <View><ReadMoreText text={instruction} maxLength={50} /></View>
     </View>
   );
 }
 function SelectByTopic(){
+  const insets = useSafeAreaInsets();
   return(
-    <View>
-      <Text>General</Text>
+    <View style = {{
+      paddingLeft: insets.left + 10,
+      paddingRight: insets.right + 10,
+      paddingTop: insets.top + 15,
+      paddingBottom: insets.bottom + 12,
+    }}>
+      <Text>Study Past Questions by topic</Text>
     </View>
   );
 }
+
+// ReadMore Text logic
+const ReadMoreText = ({ text, maxLength }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpansion = () => {
+    setExpanded(!expanded);
+  };
+
+  const renderText = () => {
+    if (text.length <= maxLength || expanded) {
+      return text;
+    }
+    return text.substring(0, maxLength) + '...';
+  };
+
+  return (
+    <View>
+      <Text>{renderText()}</Text>
+      {text.length > maxLength && (
+        <TouchableHighlight onPress={toggleExpansion}>
+          <Text style={{ color: 'blue' }}>
+            {expanded ? 'Read Less' : 'Read More'}
+          </Text>
+        </TouchableHighlight>
+      )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   homeContainer:{
